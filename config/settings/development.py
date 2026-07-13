8f82328c3676bfda1c25b6bd294e95d3b7577a2d
reehassan <theailaboratories@gmail.com>
2026-07-13 13:50:23 +0500

    feat(storage): configure django-storages for local dev and R2 production
    - development.py: local FileSystemStorage, MEDIA_ROOT
    - production.py: S3-compatible backend pointed at Cloudflare R2 endpoint
    - storage backend is swappable without touching application code

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,9 +1,20 @@
 from .base import *
 
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 DEBUG = env.bool("DEBUG", default=True)
 
 ALLOWED_HOSTS = ["*"]
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
--- config/settings/production.py
+++ config/settings/production.py
@@ -1,5 +1,14 @@
 from .base import *
 
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3.S3Storage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
+
 DEBUG = False
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
@@ -15,3 +24,11 @@ SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 
 SECURE_HSTS_PRELOAD = True
+
+
+# AWS_ACCESS_KEY_ID = env("R2_ACCESS_KEY_ID")
+# AWS_SECRET_ACCESS_KEY = env("R2_SECRET_ACCESS_KEY")
+# AWS_STORAGE_BUCKET_NAME = env("R2_BUCKET_NAME")
+# AWS_S3_ENDPOINT_URL = env("R2_ENDPOINT_URL")
+# AWS_S3_REGION_NAME = "auto"  # R2 doesn't use AWS regions, "auto" is R2's convention
+# AWS_DEFAULT_ACL = None  # R2 buckets are private by default; don't fight that
