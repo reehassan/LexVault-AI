f1c1cce7b7829f1399995922c9359a093f03095d
reehassan <theailaboratories@gmail.com>
2026-07-24 16:20:40 +0500

    fix: comment out documents.urls include until Day 14 builds it

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("documents/", include("apps.documents.urls")),
+    # path("documents/", include("apps.documents.urls")),
 ]
 
 if settings.DEBUG:
